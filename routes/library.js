11fda6a6ebe878e3ae92f7c7c51c17d7c067f352
Bob Rockers <bob@rockerssoft.org>
2018-06-16 11:41:01 -0500

    Working list of our books using mongoose and our database.

--- routes/library.js
+++ routes/library.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/library');
+var db = mongoose.connection;
+
+db.on('error', function(msg){
+	console.log("Mongoose: Unable to connect to database");
+});
+
+db.once('open', function(){
+	console.log("Mongoose connected to library datatbase.");
+});
+
+var Books = require('../models/library');
 
 router.get('/books/list', function(req, res){
-	res.json({title:"Green Eggs and Ham", author: "Dr. Seuss", status: "read"});
+	Books.find({}).exec(function(err, books){
+		if(err){
+			console.log("Error getting books from library");
+		} else {
+			res.json(books);
+		}
+	});
 });
 
 module.exports = router;
